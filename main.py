27ca2180e29394f9c3f9fda8482514e3a553b964
Dong-Jing-Yu <1055411737@qq.com>
2026-01-17 19:51:03 +0800

    更新

--- main.py
+++ main.py
@@ -15,58 +15,55 @@ class JoinLog(Star):
     async def handle_group_add(self, event: AiocqhttpMessageEvent):
         """处理加群和退群事件"""
         raw = getattr(event.message_obj, "raw_message", None)
-        if isinstance(raw, dict):
-            post_type = raw.get("post_type")
+        if not isinstance(raw, dict): return
+
+        post_type = raw.get("post_type")
+        user_id = str(raw.get("user_id"))
+        group_id = str(raw.get("group_id"))
+
+        # logger.info(f"测试: {raw}")
+
+        if post_type == "request" and raw.get("request_type") == "group":
+            sub_type = raw.get("sub_type")
+            comment = raw.get("comment", "")
+            verify_type = ""
+            if '\n' in comment:
+                verify_type = "验证消息"
+                comment = comment.split('\n')
+                if len(comment) >= 2:
+                    question = comment[0][3:]
+                    answer = comment[1][3:]
+                else:
+                    verify_type = "管理审核"
+                    question = ""
+                    answer = comment
+
+                flag = raw.get("flag")
+
+            if sub_type == "add":
+                # 加群
+                logger.info(f"收到用户 {user_id} 的加群申请")
+                logger.info(f"问题: {question}")
+                logger.info(f"回答: {answer}")
+                logger.info(f"验证方式: {verify_type}")
+                logger.info(f"请求标识: {flag}")
+
+        elif post_type == "notice":
             notice_type = raw.get("notice_type")
-            request_type = raw.get("request_type")
-            user_id = raw.get("user_id")
-            self_id = raw.get("self_id")
-            group_id = raw.get("group_id")
-            # logger.info(f"测试: {raw}")
 
-            # 确保不是自己触发的
-            if post_type == "notice" and user_id != self_id:
-                
-                # 增加
-                if notice_type == "group_increase":
-                    sub_type = raw.get("sub_type")
-                    operator_id = raw.get("operator_id")
-                    if sub_type == "approve":
-                        logger.info(f"用户 {user_id} 被管理员 {operator_id} 同意加入群 {group_id}")
-                    elif sub_type == "invite":
-                        logger.info(f"用户 {user_id} 被 {operator_id} 邀请加入群 {group_id}")
-                # 减少
-                elif notice_type == "group_decrease":
-                    sub_type = raw.get("sub_type")
-                    operator_id = raw.get("operator_id")
-                    if sub_type == "leave":
-                        logger.info(f"用户 {user_id} 主动退出群 {group_id}")
-                    elif sub_type == "kick":
-                        logger.info(f"用户 {user_id} 被管理员 {operator_id} 踢出群 {group_id}")
-            
-            # 请求
-            elif post_type == "request":
-                if request_type == "group":
-                    sub_type = raw.get("sub_type")
-                    comment = raw.get("comment", "")
-                    verify_type = ""
-                    if '\n' in comment:
-                        verify_type = "验证消息"
-                        comment = comment.split('\n')
-                        if len(comment) >= 2:
-                            question = comment[0][3:]
-                            answer = comment[1][3:]
-                    else:
-                        verify_type = "管理审核"
-                        question = ""
-                        answer = comment
-
-                    flag = raw.get("flag")
-                            
-                    if sub_type == "add":
-                        # 加群
-                        logger.info(f"收到用户 {user_id} 的加群申请")
-                        logger.info(f"问题: {question}")
-                        logger.info(f"回答: {answer}")
-                        logger.info(f"验证方式: {verify_type}")
-                        logger.info(f"请求标识: {flag}")
+            # 增加
+            if notice_type == "group_increase":
+                sub_type = raw.get("sub_type")
+                operator_id = raw.get("operator_id")
+                if sub_type == "approve":
+                    logger.info(f"用户 {user_id} 被管理员 {operator_id} 同意加入群 {group_id}")
+                elif sub_type == "invite":
+                    logger.info(f"用户 {user_id} 被 {operator_id} 邀请加入群 {group_id}")
+            # 减少
+            elif notice_type == "group_decrease":
+                sub_type = raw.get("sub_type")
+                operator_id = raw.get("operator_id")
+                if sub_type == "leave":
+                    logger.info(f"用户 {user_id} 主动退出群 {group_id}")
+                elif sub_type == "kick":
+                    logger.info(f"用户 {user_id} 被管理员 {operator_id} 踢出群 {group_id}")
